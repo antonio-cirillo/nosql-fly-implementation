cdaf8987d02b0986f7ac01b11ad2511ce25ab8eb
antonio-cirillo <dev.cirillo@gmail.com>
2021-05-03 14:13:28 +0200

    test gen code

--- JS-MongoDB/selectGenerator.js
+++ JS-MongoDB/selectGenerator.js
@@ -14,45 +14,44 @@ const func = async () => {
 	
 	const nosql = __nosqlClient.db("mydb").collection("weather");
 
-    const select = async () => {
-
-        let features = [];
-        let objects = [];
-
-        __select = await nosql.find({ }).forEach((object) => {
-            
-            const keys = Object.keys(object);
-            const n = features.length;
-            let i;
-            
-            for(i = 0; i < n; ++i) 
-                if(!(JSON.stringify(features[i]) !== JSON.stringify(keys)))
-                    break;
-                
-
-            if(i === n) {
-                features.push(keys);
-                const ___array = [];
-                ___array.push(object);
-                objects.push(___array);
-            } else
-                objects[i].push(object);
-
-        });
-
-        let tables = [];
-
-        for(i = 0; i < features.length; ++i)
-            tables.push(new __dataframe(
-                objects[i],
-                features[i]
-            ));
-
-        return tables;
-
-    }
-
-    const result = await select();
+    var statement = "{ }"
+	const select = async () => {
+	
+		let features = [];
+		let objects = [];
+		
+		await nosql.find(JSON.parse(statement)).forEach((object) => {
+			
+			const keys = Object.keys(object);
+			const n = features.length;
+			let i;
+			
+			for(i = 0; i < n; ++i)
+				if(!(JSON.stringify(features[i]) !== JSON.stringify(keys)))
+					break;
+			
+			if(i === n) {
+				features.push(keys);
+				const __array = [];
+				__array.push(object);
+				objects.push(__array);
+			} else
+				objects[i].push(object);
+			
+		});
+		
+		let tables = [];
+		
+		for(i = 0; i < features.length; ++i)
+			tables.push(new __dataframe(
+				objects[i],
+				features[i]
+			));
+			
+		return tables;
+	}
+	
+	var result = (await select()); // => TEST PASSED! -> Retrive list of dataframe from select operation.
 
     for(const table of result) {
         table.show();
