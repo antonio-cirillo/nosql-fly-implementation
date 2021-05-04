1fe10461abc9068c3b057b4298f56220993d3701
antonio-cirillo <dev.cirillo@gmail.com>
2021-05-04 18:06:53 +0200

    test delete query node gen

---
+++ JS-MongoDB/deleteGenerator.js
@@ -0,0 +1,64 @@
+var __dataframe = require("dataframe-js").DataFrame;
+var __nosql = require("mongodb");
+var __fs = require("fs")
+var __parse = require("csv-parse");
+
+const func = async () => {
+
+	const __nosqlClient = new __nosql.MongoClient(
+		"mongodb://127.0.0.1:27017/",
+		{ useUnifiedTopology: true }
+	);
+	
+	await __nosqlClient.connect();
+	
+	const nosql = __nosqlClient.db("mydb").collection("weather");
+	var pathOfCSV = "/home/devcirillo/Documenti/GitHub/nosql-fly-implementation/FLY-MongoDB/weatherHistory.csv"
+	/* var weatherCSVClient = containerClient.getBlobClient(pathOfCSV);
+	var weatherCSV = await weatherCSVClient.download(); */ // -> Fly code gen.
+
+	const insertWeatherCSV = async () => {
+		
+		let i = 0;
+		let features;
+		let objects = [];
+		
+		await new Promise((resolve) => {
+		
+			__fs.createReadStream(pathOfCSV)
+			.pipe(__parse())
+			.on("data", (row) => {
+				if(i == 0) {
+					features = row;
+					++i;
+				} else if(i >= 1 && i <= 10) {
+					let object = { };
+					for([index, value] of features.entries())
+						object[features[index]] = row[index];
+					objects.push(object);
+					++i;
+				} else if(i < 1)
+					++i;
+			})
+			.on("end", () => {
+				resolve();
+			});
+		});
+		
+		return objects;
+	}
+	
+	await nosql.insertMany((await insertWeatherCSV()));
+    
+    const deleteQuery = JSON.parse("{ }")
+	
+	var result = (await nosql.deleteMany(deleteQuery)).deletedCount // => TEST PASSED! Delete instance inside DB and return number of instance eliminated.
+	
+	if(result > 0)
+		console.log("One or more item(s) was eliminated") 
+    
+    await __nosqlClient.close(); 
+
+}
+
+func(); 
