17c03b4b97300a5ea728233928684da12b2a59bf
antonio-cirillo <dev.cirillo@gmail.com>
2021-05-10 13:22:28 +0200

    test crud operation

--- PY-MongoDB/app.py
+++ PY-MongoDB/app.py
@@ -2,5 +2,32 @@ from pymongo import MongoClient
 
 client = MongoClient("mongodb://127.0.0.1:27017/")
 
-db = client.test
-print(db)
+db = client['mydb']
+
+collection = db['weather']
+
+students = [{
+    "name": 'Antonio', 
+    "surname": 'Cirillo',
+    "age": 21, 
+    "address": {
+        "city": 'Battipaglia',
+        "zip": 84091,
+        "street": 'Via Paolo Baratta'
+    },
+    "tel": ['333-xxxxxx']
+}]
+
+collection.insert_many(students)
+
+collection.update_many({ "name": 'Antonio' }, { "$set": { "address.city": 'Salerno' } })
+
+for post in collection.find({}):
+    print(post)
+
+collection.replace_one({ "name" : 'Antonio' }, { "replaced": 'true' })
+
+for post in collection.find({}):
+    print(post)
+
+collection.delete_many({})
