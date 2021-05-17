180bcef2703b150090af0b9b933026c573958f40
antonio-cirillo <dev.cirillo@gmail.com>
2021-05-17 15:27:49 +0200

    retrieve list of dataframe from select query

--- PY-MongoDB/app.py
+++ PY-MongoDB/app.py
@@ -1,5 +1,8 @@
+from numpy import array
 from pymongo import MongoClient
 import json
+import pandas as pd
+from pymongo.results import InsertManyResult
 
 client = MongoClient("mongodb://127.0.0.1:27017/")
 
@@ -7,6 +10,7 @@ db = client['mydb']
 
 collection = db['weather']
 
+'''
 studentsString = "{\"name\": \"Antonio\", \"surname\": \"Cirillo\", \"age\": 21, \"address\": { \"city\": \"Battipaglia\", \"zip\": 84091, \"street\": \"Via Paolo Baratta\" } }"
 
 if studentsString[0] != "[":
@@ -15,11 +19,8 @@ else :
     print()
 
 y = json.loads(studentsString)
-
-print(studentsString)
-
-
 '''
+
 students = [{
     "name": 'Antonio', 
     "surname": 'Cirillo',
@@ -34,6 +35,7 @@ students = [{
 
 collection.insert_many(students)
 
+'''
 collection.update_many({ "name": 'Antonio' }, { "$set": { "address.city": 'Salerno' } })
 
 for post in collection.find({}):
@@ -46,3 +48,39 @@ for post in collection.find({}):
 
 collection.delete_many({})
 '''
+
+file = pd.read_csv("C:\\Users\\devci\\Documents\\GitHub\\nosql-fly-implementation\\FLY-MongoDB\\weatherHistory.csv", skiprows = range(1,4), nrows = 2)
+
+collection.insert_many(file.to_dict('records'))
+
+def __select__():
+    result = collection.find({})
+    features = []
+    objects = []
+    for value in result:
+        feature = value.keys()
+        i = 0
+        for f in features:
+            if f == feature:
+                break
+            else:
+                i = i + 1 
+        if i + 1 > len(features):
+            features.append(feature)
+            objects.append([])
+            objects[len(objects) - 1].append(value)
+        else:
+            objects[i].append(value)
+
+    df = []
+    for i in range(len(features)):
+        df.append(pd.DataFrame(objects[i]))
+
+    return df
+
+df = __select__()
+
+for value in df:
+    print(value)
+
+collection.delete_many({})
